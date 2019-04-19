7d9d0573511a4a9b18a6a8498dad47df279dfa57
littleghostprince <ashleysmejia@gmail.com>
2019-04-19 09:57:18 -0600

    4/19
    SoundButton class and Game class

---
+++ Nim Game/Assets/Script/Game.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] GameObject Main_Menu = null;
+    [SerializeField] GameObject Main_Game = null;
+    [SerializeField] GameObject End_Prompt = null;
+    [SerializeField] GameObject End_Prompt_2 = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
