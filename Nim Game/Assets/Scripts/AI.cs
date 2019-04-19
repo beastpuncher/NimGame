d32b1e8bd0e2115c37a482f57d264cc64d1feacc
whatifidontwantto <BootyFister4>
2019-04-19 09:36:55 -0600

    Finished definitions and Player half AI

---
+++ Nim Game/Assets/Scripts/AI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : Player
+{
+    public List<UI> buttons = null;
+
+    public bool RandomlyPickStuff()
+    {
+
+        return false;
+    }
+}
