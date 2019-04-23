00b999b71d30488d6b49ada1a319a769c3d88c27
littleghostprince <ashleysmejia@gmail.com>
2019-04-23 09:43:33 -0600

    4/23
    sdkjfdlkn

--- Nim Game/Assets/Script/Game.cs
+++ Nim Game/Assets/Script/Game.cs
@@ -8,16 +8,19 @@ public class Game : MonoBehaviour
     [SerializeField] GameObject Main_Game = null;
     [SerializeField] GameObject End_Prompt = null;
     [SerializeField] GameObject End_Prompt_2 = null;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+
     }
 
 }
