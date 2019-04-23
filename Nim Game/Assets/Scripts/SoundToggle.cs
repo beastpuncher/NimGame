bdfd18c806173b6041c0397de44ca70d93ca95c6
beastpuncher <beastpuncher@gmail.com>
2019-04-23 09:28:31 -0600

    Added Mute On Sound Button

---
+++ Nim Game/Assets/Scripts/SoundToggle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundToggle : MonoBehaviour
+{
+    [SerializeField] AudioMixer audioMixer = null;
+    [SerializeField] bool isOn = true;
+    
+    public void OnClick()
+    {
+        isOn = !isOn;
+        if (isOn)
+        {
+            audioMixer.SetFloat("masterVol", -80.0f);
+        }
+        else
+        {
+            audioMixer.SetFloat("masterVol", 0.0f);
+        }
+    }
+}
