fb6d1e7b3f1ad5ebb16e9e8da421a5f200687c85
littleghostprince <ashleysmejia@gmail.com>
2019-04-24 12:47:58 -0600

    4/24
    Updaetslkljfa/;sf

---
+++ Nim Game/Assets/Scripts/Game.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] GameObject Main_Menu = null;
+    [SerializeField] GameObject Main_Game = null;
+    [SerializeField] GameObject End_Prompt = null;
+    [SerializeField] GameObject End_Prompt_2 = null;
+    [SerializeField] TMP_Dropdown dif = null;
+    [SerializeField] TMP_Dropdown mode = null;
+    [SerializeField] TMP_Dropdown win = null;
+    [SerializeField] TextMeshProUGUI m_p1Name = null;
+    [SerializeField] TextMeshProUGUI m_p2Name = null;
+    public Definitions.eDiffcultyenum difficulty;
+    public Definitions.eGameMode gameMode;
+    public Definitions.eWinCondition winCon;
+    Player m_player1 = new Player();
+    Player m_player2;
+
+    void Start()
+    {
+        Main_Menu.SetActive(true);
+        Main_Game.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        Main_Menu.SetActive(false);
+        Main_Game.SetActive(true);
+        difficulty = (Definitions.eDiffcultyenum)dif.value;
+        gameMode = (Definitions.eGameMode)mode.value;
+        winCon = (Definitions.eWinCondition)win.value;
+        if (gameMode == Definitions.eGameMode.PLAYER_VS_PLAYER)
+        {
+            m_player2 = new Player();
+        }
+        else
+        {
+            m_player2 = new AI();
+        }
+        m_player1.name = m_p1Name.text.ToString();
+        m_player2.name = m_p2Name.text.ToString();
+        Debug.Log(m_player1.name);
+    }
+
+    void Update()
+    {
+       
+
+    }
+
+}
--- Nim Game/Assets/Scripts/UI.cs
+++ Nim Game/Assets/Scripts/UI.cs
@@ -5,29 +5,94 @@ using TMPro;
 
 public class UI : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI m_titleText = null;
-    [SerializeField] List<RowButton> rowButtons = new List<RowButton>();
-    [SerializeField] List<GameObject> optionButtons = new List<GameObject>();
+    [SerializeField] Game game = null;
+    [SerializeField] GameObject gamePiece = null;
+    [SerializeField] GameObject rowBut = null;
+    [SerializeField] List<GameObject> rowPlaceholder = null;
+    [SerializeField] List<GameObject> rowButtonPlaceholder = null;
+    List<GameObject> rowButtons = new List<GameObject>();
+    List<GameObject> optionButtons = new List<GameObject>();
 
     private void Start()
     {
-        foreach (var button in rowButtons)
+       
+    }
+
+    public void GameStarted()
+    {
+        switch (game.difficulty)
         {
-            foreach (var piece in button.rowPieces)
-            {
-                piece.SetActive(true);
-            }
+            case Definitions.eDiffcultyenum.EASY:
+                for (int i = 0; i < 2; i++)
+                {
+                    rowButtons.Add(rowBut);
+                    for (int j = 0; j < 3; j++)
+                    {
+                        rowButtons[i].GetComponent<RowButton>().rowPieces.Add(gamePiece);
+                    }
+                }
+                break;
+            case Definitions.eDiffcultyenum.MEDIUM:
+                for (int i = 0; i < 3; i++)
+                {
+                    rowButtons.Add(rowBut);
+                    int x = 0;
+                    if (i == 0)
+                    {
+                        x = 2;
+                    }
+                    else if (i == 1)
+                    {
+                        x = 5;
+                    }
+                    else
+                    {
+                        x = 7;
+                    }
+                    for (int j = 0; j < x; j++)
+                    {
+                        rowButtons[i].GetComponent<RowButton>().rowPieces.Add(gamePiece);
+                    }
+                }
+                break;
+            case Definitions.eDiffcultyenum.HARD:
+                for (int i = 0; i < 4; i++)
+                {
+                    rowButtons.Add(rowBut);
+                    int x = 0;
+                    if (i == 0)
+                    {
+                        x = 2;
+                    }
+                    else if (i == 1)
+                    {
+                        x = 3;
+                    }
+                    else if (i == 2)
+                    {
+                        x = 8;
+                    }
+                    else
+                    {
+                        x = 9;
+                    }
+                    for (int j = 0; j < x; j++)
+                    {
+                        rowButtons[i].GetComponent<RowButton>().rowPieces.Add(gamePiece);
+                    }
+                }
+                break;
+        }
+        Debug.Log(rowButtons.Count);
+        for(int i = 0; i<rowButtons.Count-1; i++)
+        {
+            Vector3 position = rowButtonPlaceholder[i].transform.position;
+            Instantiate(rowButtons[i], position, Quaternion.identity);
         }
     }
 
     private void Update()
     {
-        foreach(var button in rowButtons)
-        {
-            foreach(var piece in button.rowPieces)
-            {
-                piece.SetActive(true);
-            }
-        }
+        
     }
 }
