16aba3bba7158fb4ce842b7a2491c7b5285e1768
littleghostprince <ashleysmejia@gmail.com>
2019-04-25 10:12:04 -0600

    4/25/19
    asjfhaskf

--- Nim Game/Assets/Scripts/Surrender.cs
+++ Nim Game/Assets/Scripts/Surrender.cs
@@ -1,13 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SurrenderButton : MonoBehaviour
+public class Surrender : MonoBehaviour
 {
-    public string Name;
+    [SerializeField] GameObject m_first_end = null;
+    [SerializeField] GameObject m_second_end = null;
 
-    private bool Quit(string playerName)
+    public void clickSurrender()
     {
-        throw new System.NotImplementedException();
+        if(m_first_end.activeSelf == false)
+        {
+            m_first_end.SetActive(true);
+        }
+    }
+    public void FirstYes()
+    {
+        if(m_second_end.activeSelf == false)
+        {
+            m_first_end.SetActive(false);
+            m_second_end.SetActive(true);
+        }
+    }
+    public void FirstNo()
+    {
+        if(m_first_end.activeSelf == true)
+        {
+            m_first_end.SetActive(false);
+        }
+    }
+
+    public void SecondYes()
+    {
+        SceneManager.LoadScene("NimGame");
+        //reload scene and start in begining
+    }
+    public void SecondNo()
+    {
+        if(m_second_end.activeSelf == true)
+        {
+            m_second_end.SetActive(false);
+        }
     }
 }
